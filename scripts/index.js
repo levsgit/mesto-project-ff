33d2d7318c7b7f9f1b793ad184c7b37a723135f6
levsgit <shatrov.lev@mail.ru>
2024-05-28 15:34:24 +0400

    Bug fixes secod attempt

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,24 @@
-let deleteCard = function (e) {
+const cardTemplate = document.querySelector('#card-template').content;
+const cardsList = document.querySelector('.places__list');
+
+const deleteCard = function (e) {
   const listItem = e.target.closest('.places__item');
   listItem.remove();
 }
 
-function addCard(card, deleteFunc) {
-  const cardTemplate = document.querySelector('#card-template').content;
+function createCard(card, deleteFunc) {
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-  cardElement.querySelector('.card__image').src = card.link;
-  cardElement.querySelector('.card__title').textContent = card.name;
+  const link = card.link;
+  const name = card.name;
+
+  cardElement.querySelector('.card__image').src = link;
+  cardElement.querySelector('.card__image').alt = name;
+  cardElement.querySelector('.card__title').textContent = name;
   cardElement.querySelector('.card__delete-button').addEventListener('click', deleteFunc);
+
   return cardElement;
 }
 
 initialCards.forEach(item => {
-  document.querySelector('.places__list').append(addCard(item, deleteCard));
+  cardsList.append(createCard(item, deleteCard));
 });
